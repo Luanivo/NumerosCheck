23190da7171541e0ad4f3d5acf19fbb42151d7ca
Luan <164570914+Luanivo@users.noreply.github.com>
2024-10-09 13:27:41 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,19 +1,68 @@
-const verificarParOuImpar = () => {
-    const numero = parseInt(document.getElementById('numero').value);
+let contador = 0;
+let input = document.getElementById("inputTarefa");
+let btnAdd = document.getElementById("btn-add");
+let main = document.getElementById("areaLista");
 
-    if (isNaN(numero)) {
-        document.getElementById('resultado').innerText = 'Por favor, digite um número válido.';
-        return;
-    }
+function addTarefa() {
+  //PEGAR O VALOR DIGITADO NO INPUT
+  let valorInput = input.value;
 
-    const resultado = numero % 2 === 0 ? `O número ${numero} é par.` : `O número ${numero} é ímpar.`;
+  //SE NÃO FOR VAZIO, NEM NULO, NEM INDEFINIDO
+  if (valorInput !== "" && valorInput !== null && valorInput !== undefined) {
+    ++contador;
 
-    document.getElementById('resultado').innerText = resultado;
-};
+    let novoItem = `<div id="${contador}" class="item">
+        <div onclick="marcarTarefa(${contador})" class="item-icone">
+            <i id="icone_${contador}" class="mdi mdi-circle-outline"></i>
+        </div>
+        <div onclick="marcarTarefa(${contador})" class="item-nome">
+            ${valorInput}
+        </div>
+        <div class="item-botao">
+            <button onclick="deletar(${contador})" class="delete"><i class="mdi mdi-delete"></i> Deletar</button>
+        </div>
+    </div>`;
 
-// Evento para pressionar Enter
-document.getElementById('numero').addEventListener('keypress', (event) => {
-    if (event.key === 'Enter') {
-        verificarParOuImpar();
-    }
+    //ADICIONAR NOVO ITEM NO MAIN
+    main.innerHTML += novoItem;
+
+    //ZERAR OS CAMPINHOS
+    input.value = "";
+    input.focus();
+  }
+}
+
+function deletar(id) {
+  var tarefa = document.getElementById(id);
+  tarefa.remove();
+}
+
+function marcarTarefa(id) {
+  var item = document.getElementById(id);
+  var classe = item.getAttribute("class");
+  console.log(classe);
+
+  if (classe == "item") {
+    item.classList.add("clicado");
+
+    var icone = document.getElementById("icone_" + id);
+    icone.classList.remove("mdi-circle-outline");
+    icone.classList.add("mdi-check-circle");
+
+    item.parentNode.appendChild(item);
+  } else {
+    item.classList.remove("clicado");
+
+    var icone = document.getElementById("icone_" + id);
+    icone.classList.remove("mdi-check-circle");
+    icone.classList.add("mdi-circle-outline");
+  }
+}
+
+input.addEventListener("keyup", function (event) {
+  //SE TECLOU ENTER (13)
+  if (event.keyCode === 13) {
+    event.preventDefault();
+    btnAdd.click();
+  }
 });
